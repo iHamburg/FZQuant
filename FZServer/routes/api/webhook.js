fe02b983ddf14e6f73b50305ff568f21d8568904
Forest <yiqing.zhang@bl.com>
2017-11-15 14:06:35 +0800

    888

--- FZServer/routes/api/webhook.js
+++ FZServer/routes/api/webhook.js
@@ -40,7 +40,7 @@ router.post('/test', function (req, res, next) {
 router.get('/', function (req, res, next) {
     // var id = req.params.id;
 
-  return res.send('Test 1223 234234 555 666 777');
+  return res.send('Test 1223 234234 555 666 777 888');
 });
 
 module.exports = router;
