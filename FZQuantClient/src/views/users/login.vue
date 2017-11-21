ae39b49091741a09740ac67aca349f2b4e07a57d
Forest <yiqing.zhang@bl.com>
2017-11-21 17:26:29 +0800

    update client

--- FZQuantClient/src/views/users/login.vue
+++ FZQuantClient/src/views/users/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h2>用户登录</h2>
+    <h2>用户登录22</h2>
     <b-form @submit="onSubmit">
       <b-form-group id="exampleInputGroup1"
                     label="用户名:" label-for="exampleInput1"
