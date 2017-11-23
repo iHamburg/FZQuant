a5d8ce9d384ba0f2ea339de2e75a6d31309dcab9
iHamburg <tominfrankfurt@gmail.com>
2017-11-23 08:12:51 +0800

    update

--- FZPython/demo/main.py
+++ FZPython/demo/main.py
@@ -2,4 +2,7 @@
 
 from pyquant.util import *
 
+from pyquant import logger
+
+logger.error('It is err')
 print(int2time(21223223))
--- FZPython/pyquant/libs/loglib.py
+++ FZPython/pyquant/libs/loglib.py
@@ -2,18 +2,27 @@
 
 from logbook import Logger, StreamHandler, FileHandler
 import sys
+# import os
+# StreamHandler(sys.stdout).push_application()
+FileHandler('./pyquant/logs/app.log').push_application()  # 会覆盖上一个Handler
 
-StreamHandler(sys.stdout).push_application()
-FileHandler('app.log').push_application()  # 会覆盖上一个Handler
-
-# log = Logger('Logbook')
+# logger = Logger('fzquant')
 # log.info('Hello, World!')
 # log.error('it is err')
 
-def print_all(text):
-    print(text)
 
+try:
+    from logbook import Logger
+except ImportError:
+    class Logger(object):
+        def __init__(self, name, level=0):
+            self.name = name
+            self.level = level
+        debug = info = warn = warning = notice = error = exception = \
+            critical = log = lambda *a, **kw: None
+
+log = Logger('pyquant')
 
 
 if __name__ == '__main__':
-    print('Let us play  python')
+    pass
